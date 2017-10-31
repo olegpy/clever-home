0888800c6538f8f8300ed5c78884009920fbab17
Oleg Kriklivets <olegkriklivets@gmail.com>
2017-10-31 10:20:37 +0200

    add new project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('test');
+let [x,y] = [1,2];
+console.log(x+y);
+let af = () => console.log('arrow func');
+let name = 'Tom';
+let age = 24;
+let res = `Name: ${name}, year ${age}`;
+console.log(res);
